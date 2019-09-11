c2438b078de38c1cb23c4635ade61841dd0db6b4
Fabio Kopezinski <fabiokopezinski@gmail.com>
2019-09-11 19:29:39 -0300

    matrix

--- software/tests/matrix/test/test.c
+++ software/tests/matrix/test/test.c
@@ -12,78 +12,51 @@ just change the variable MATRIX_TYPE in the makefile
 // list of individual tests
 //void sqrt1_test();
 //void sqrt4_test();
-void super_mat_test();
+void sub_matrix_teste1();
+void sum_matrix_teste2();
 
 // main test
 void hfunit_run_tests(){
-	super_mat_test();
+	sub_matrix_teste1();
+	sum_matrix_teste2();
 }
 
-// place here a nice description for each test
-// eu peguei um exemplao só p vcs verem como usar as funcoes
-// mas vcs vao ter q separar bem os teste. nao pode fazer assim !!!
-void super_mat_test() {
 
-struct Matrix M1, M2, M3, M4, M5, M6, M7;
-typ_var Multiplicator = val(5);
+void sub_matrix_teste1() {
+
+struct Matrix M1,M5;
+
+typ_var val1[16] = {val(1),val(2),val(3),val(4),val(5),val(6),val(7),val(8),val(9),val(10),val(11),val(12),val(13),val(14),val(15),val(16)};
+//typ_var val2[16] = {val(2),val(3),val(4),val(5),val(6),val(7),val(8),val(9),val(10),val(11),val(12),val(13),val(14),val(15),val(16),val(17)};
+M1 = set_values(4, 4, val1);
+//M2 = set_values(4, 4, val2);
+
+printf("Matrix 1 is:\n");
+print_matrix(M1);
+printf("Matrix 2 is:\n");
+print_matrix(M1);
+printf("Subtraction of Matrix 1 and 2 is:\n");
+M5 = subtraction(M1, M1);
+print_matrix(M5);
+hfunit_comp_vector(M5,M1,16,"subtraction(M1,M1)");
+
+}
+
+void sum_matrix_teste2() {
+
+struct Matrix M1,M2,M5;
 
 typ_var val1[16] = {val(1),val(2),val(3),val(4),val(5),val(6),val(7),val(8),val(9),val(10),val(11),val(12),val(13),val(14),val(15),val(16)};
 typ_var val2[16] = {val(2),val(3),val(4),val(5),val(6),val(7),val(8),val(9),val(10),val(11),val(12),val(13),val(14),val(15),val(16),val(17)};
-typ_var val3[16] = {val(4),val(2),val(2),val(2),val(0),val(1),val(-3),val(3),val(0),val(-1),val(3),val(3),val(0),val(3),val(1),val(1)};
-
 M1 = set_values(4, 4, val1);
 M2 = set_values(4, 4, val2);
-M3 = set_values(4, 4, val3);
-
-//----------------------------FUNCTIONS TESTS-----------------------------------
 
 printf("Matrix 1 is:\n");
 print_matrix(M1);
 printf("Matrix 2 is:\n");
 print_matrix(M2);
-printf("Matrix 3 is:\n");
-print_matrix(M3);
-printf("Transposed of Matrix 1 is:\n");
-M4 = transposed(M1);
-print_matrix(M4);
 printf("Sum of Matrix 1 and 2 is:\n");
-M5 = sum(M1, M2);
+M5 = sum(M1, M1);
 print_matrix(M5);
-printf("Subtraction of Matrix 1 and 2 is:\n");
-M5 = subtraction(M1, M2);
-print_matrix(M5);
-printf("Multiplication of Matrix 1 and 2 is:\n");
-M5 = multiplication(M1, M2);
-print_matrix(M5);
-printf("Multiplication of Matrix 1 by ");
-SHOW(Multiplicator);
-printf(":\n");
-M5 = multE(M1, Multiplicator);
-print_matrix(M5);
-printf("Division of Matrix 1 by ");
-SHOW(Multiplicator);
-printf(":\n");
-M5 = divE(M1, Multiplicator);
-print_matrix(M5);
-printf("Inverse of Matrix 3 is:\n");
-M5 = Invert(M3);
-print_matrix(M5);
-printf("Matrix of zeros of size %d by %d:\n", 4, 4);
-M5 = zeros(4, 4);
-print_matrix(M5);
-printf("Identity Matrix of size %d by %d:\n", 4, 4);
-M5 = setEye(4);
-print_matrix(M5);
-printf("Matrix of ones of size %d by %d:\n", 3, 3);
-M6 = ones(3, 3);
-print_matrix(M6);
-printf("Diagonal Block Concatenation of Two Matrices:\n");
-M7 = blkdiag2(M1, M2);
-print_matrix(M7);
-int conf[4] = {2,2,8,8};
-M7 =  customMat(4, conf, M1, M2, M3, M5);
-printf("Custom Matrix Made of Four Matrices:\n");
-print_matrix(M7);
-
 
 }
